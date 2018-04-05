0d5af5c0dbd4e943d7ad5161004c70e1bc3ba7aa
Vitaly Volovodenko <volovodenko@gmail.com>
2018-04-05 12:50:28 +0300

    Patch 2.0

--- js/common.js
+++ js/common.js
@@ -1,38 +1,40 @@
 "use strict";
 
 function Component(obj) {
-
     for (var prop in obj) {
-        this[prop] = obj[prop];
+        if (obj.hasOwnProperty(prop)) {
+            this[prop] = obj[prop];
+        }
     }
 }
 
-Component.prototype.setView = function (view, data = null) {
+Component.prototype.setView = function (view, data) {
     this.htmlView = view;
-    data ? this.data = data : null;
+    if (data) {
+        this.data = data;
+    }
 };
 
 Component.prototype.render = function () {
-
     this.component = document.createElement(this.parent);
     this.component.innerHTML = this.htmlView.replace(/{\w*}/g, replacer.bind(this));
 
     return this.component;
+};
 
-    function replacer(str) {
-        str = str.replace(/{|}/g, "");
+function replacer(str) {
+    str = str.replace(/{|}/g, "");
 
-        switch (this.parent) {
-            case "header":
-            case "footer":
-                return this[str];
-                break;
+    switch (this.parent) {
+        case "header":
+        case "footer":
+            return this[str];
+            break;
 
-            case "nav" :
-                return this.renderSubItem(this.data);
-                break;
-        }
-    };
+        case "nav" :
+            return this.renderSubItem(this.data);
+            break;
+    }
 };
 
 Component.prototype.delete = function () {
@@ -41,15 +43,16 @@ Component.prototype.delete = function () {
 
 Component.prototype.renderSubItem = function (obj) {
     var item = "";
+    var subitem;
 
     for (var key in obj) {
-        var subitem = "";
+        subitem = "";
 
-        if (obj[key]['items']) {
+        if (obj[key]["items"]) {
             subitem = "<ul class='submenu'>" + this.renderSubItem(obj[key]['items']) + "</ul>";
         }
 
-        item += "<li><a href=" + obj[key]['url'] + ">" + obj[key]['name'] + "</a>" + subitem + "</li>";
+        item += "<li><a href=" + obj[key]["url"] + ">" + obj[key]["name"] + "</a>" + subitem + "</li>";
     }
 
     return item;
@@ -58,46 +61,46 @@ Component.prototype.renderSubItem = function (obj) {
 //////////////////////////////////////////////
 /////////////////////////////////////////////
 
-var componentHeader = new Component({parent: 'header', url: './img/logo.png', title: 'Рога и Копыта'}),
-    componentMenu = new Component({parent: 'nav'}),
-    componentFooter = new Component({parent: 'footer', text: '&#169; Копирайты'}),
-    viewHeader = '<h1><img src="{url}" alt="{title}"/>{title}</h1>',
-    viewMenu = '<ul>{li}</ul>',
-    viewFooter = '<p><small>{text}</small</p>',
-    dataMenu = [
-        {
-            name: 'Главная',
-            url: '#'
-        },
-        {
-            name: 'O нас',
-            url: '#',
-            items: [
-                {
-                    name: 'Кто мы', url: '#', items: [
-                        {
-                            name: 'Рога', url: '#', items: [
-                                {name: 'Большие', url: '#'},
-                                {name: 'Маленькие', url: '#'}
-                            ]
-                        },
-                        {
-                            name: 'Копыта', url: '#', items: [
-                                {name: 'Парные', url: '#'},
-                                {name: 'Непарные', url: '#'}
-                            ]
-                        }
-                    ]
-                },
-                {name: 'Где мы', url: '#'},
-                {name: 'Откуда', url: '#'}
-            ]
-        },
-        {
-            name: 'Контакты',
-            url: '#'
-        }
-    ];
+var componentHeader = new Component({parent: 'header', url: './img/logo.png', title: 'Рога и Копыта'});
+var componentMenu = new Component({parent: 'nav'});
+var componentFooter = new Component({parent: 'footer', text: '&#169; Копирайты'});
+var viewHeader = '<h1><img src="{url}" alt="{title}"/>{title}</h1>';
+var viewMenu = '<ul>{li}</ul>';
+var viewFooter = '<p><small>{text}</small</p>';
+var dataMenu = [
+    {
+        name: 'Главная',
+        url: '#'
+    },
+    {
+        name: 'O нас',
+        url: '#',
+        items: [
+            {
+                name: 'Кто мы', url: '#', items: [
+                    {
+                        name: 'Рога', url: '#', items: [
+                            {name: 'Большие', url: '#'},
+                            {name: 'Маленькие', url: '#'}
+                        ]
+                    },
+                    {
+                        name: 'Копыта', url: '#', items: [
+                            {name: 'Парные', url: '#'},
+                            {name: 'Непарные', url: '#'}
+                        ]
+                    }
+                ]
+            },
+            {name: 'Где мы', url: '#'},
+            {name: 'Откуда', url: '#'}
+        ]
+    },
+    {
+        name: 'Контакты',
+        url: '#'
+    }
+];
 
 componentHeader.setView(viewHeader);
 componentMenu.setView(viewMenu, dataMenu);
